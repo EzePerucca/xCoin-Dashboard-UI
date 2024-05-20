6728bc31bee84bfbd1f1e9dbb12a04738a3350ca
EzePerucca <eze.perucca@hotmail.com>
2024-05-20 15:45:40 -0300

    Environment files added

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from './../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://localhost:3000/historicTransactions';
-
   constructor(private http: HttpClient) { }
 
   getTransactionHistory(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}`, {
+    return this.http.get<any>(`${environment.apiUrl}/historicTransactions`, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
