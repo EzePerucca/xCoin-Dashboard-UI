9ac58db69b5347b147809e8acebd5cf3ee66705f
EzePerucca <eze.perucca@hotmail.com>
2024-05-24 15:17:18 -0300

    Environment change to PROD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'xapp-alb-439058958.us-east-1.elb.amazonaws.com'
+    apiUrl: 'http://xapp-alb-439058958.us-east-1.elb.amazonaws.com'
 };
